9b27f3f9d405d053593ef7a54a5ac2c4a3fc7b10
Alexandre Huat <alexandre.huat@gmail.com>
2023-06-22 00:28:40 +0200

    fix JS nav bar

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -4,12 +4,12 @@ function createNavList(level = "h2") {
     const navList = document.getElementById("nav-list");
     document.querySelectorAll(level).forEach(function (item) {
         const a = document.createElement("a");
-        a.classList.add("buttons");
+        // a.classList.add("buttons");
         a.href = "#" + item.id;
-        const text = document.createTextNode(item.innerHtml);
+        const text = document.createTextNode(item.innerHTML);
         a.appendChild(text);
         const li = document.createElement("li");
         li.appendChild(a);
-        navList.appendChild(a);
+        navList.appendChild(li);
     });
 }
