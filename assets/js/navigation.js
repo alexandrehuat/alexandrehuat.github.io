921cb780b0636c9ea175d2ecf059840068ff2f78
Alexandre Huat <alexandre.huat@gmail.com>
2023-06-22 00:19:55 +0200

    fix JS nav bar

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", createNavList)
+document.addEventListener("DOMContentLoaded", () => createNavList())
 
 function createNavList(level = "h2") {
     const navList = document.getElementById("nav-list");
     document.querySelectorAll(level).forEach(function (item) {
         const a = document.createElement("a");
+        a.classList.add("buttons");
         a.href = "#" + item.id;
         const text = document.createTextNode(item.innerHtml);
         a.appendChild(text);
