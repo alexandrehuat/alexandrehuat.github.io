0a4085e7f6df0b29a445a4db74682f2439ec8372
Alexandre Huat <alexandre.huat@gmail.com>
2023-06-22 00:32:38 +0200

    update JS nav bar

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -4,9 +4,9 @@ function createNavList(level = "h2") {
     const navList = document.getElementById("nav-list");
     document.querySelectorAll(level).forEach(function (item) {
         const a = document.createElement("a");
-        // a.classList.add("buttons");
+        a.classList.add("buttons");
         a.href = "#" + item.id;
-        const text = document.createTextNode(item.innerHTML);
+        const text = document.createTextNode(item.textContent);
         a.appendChild(text);
         const li = document.createElement("li");
         li.appendChild(a);
